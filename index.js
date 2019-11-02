9bd654575e51e0ee115877ee543f9535f9fc62c1
Clarion Heard <clahon54@gmail.com>
2019-11-02 16:30:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ whisper(string) {
 }
 
 function
+logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function
+logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function
+sayHiToGrandma(string) {
+  
+}
