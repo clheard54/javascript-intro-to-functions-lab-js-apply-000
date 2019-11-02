c598a08c8b1663ed6c0b6ff57394c4d930ab7224
Clarion Heard <clahon54@gmail.com>
2019-11-02 16:37:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ logWhisper(string) {
 
 function
 sayHiToGrandma(string) {
-  if string === "I love you, Grandma" {
-    return "I love you,too."
+  if (string === "I love you, Grandma") {
+    return "I love you,too.";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!";
   }
-  else if string.toUpperCase() === string {
-    return "YES INDEED!"
-  }
-  else if string.toLowerCase
 
 }
